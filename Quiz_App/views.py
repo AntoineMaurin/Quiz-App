06d17b27f540e744ca9fb6ec15ad7c68c5971c19
Antoine Maurin <antoine.maurin26@gmail.com>
2021-01-31 19:03:26 +0100

    Préparation au rendu des réponses justes et fausses à la fin d'un quiz en changeant seulement une classe, et réparation du problème de décalage de question dans le dictionnaire contenant les résultats du quiz (et détail du nouveau fonctionnement de la vue).

--- Quiz_App/views.py
+++ Quiz_App/views.py
@@ -21,7 +21,7 @@ def startquizpage(request):
     ids_list = []
     for question in questions:
         ids_list.append(question.id)
-    request.session['question_number'] = 0
+    request.session['question_number'] = 1
     request.session['questions_left'] = ids_list
     request.session['quiz_id'] = quiz.id
 
@@ -32,32 +32,47 @@ def nextquestionpage(request):
     quiz_id = request.session['quiz_id']
     quiz = Quiz.objects.get(id=quiz_id)
 
+    # Tests if there is still questions left
     try:
+        # Gets the first question of the list, gets the real Question object,
+        # and gets the list of answers checked.
         current_question_id = request.session['questions_left'][0]
         current_question = Question.objects.get(id=current_question_id)
         checked_answers = request.POST.getlist('is_answer_checked')
 
-
+        # Builds a dictionnary that will store the answers of each question
         if 'quiz_results' in request.session:
+
+            request.session['question_number'] += 1
+            # Each time this view is called, it renders the first question of
+            # the list, so here i delete the last question played
+            del(request.session['questions_left'][0])
+            request.session.modified = True
+
+            # Here i add the question as a key and the checked answers as value
+            # in the 'quiz_resulst' dictionnary that i pass in the session dict
             request.session['quiz_results'][str(current_question.title)] = checked_answers
+
+            # Updates the list of questions left for the next one
+            current_question_id = request.session['questions_left'][0]
+            current_question = Question.objects.get(id=current_question_id)
+
+        # If the dictionnary doesn't exist, create it (before the 1st question)
         else:
             request.session['quiz_results'] = {}
-
-        request.session['question_number'] += 1
-        del(request.session['questions_left'][0])
-        request.session.modified = True
 
         return render(request, "quiz_question_playing.html", {'quiz': quiz,
                                                               'current_question': current_question,
                                                               'question_number': request.session['question_number'],
                                                               })
+    # If there is no question left, generates the quiz_resulst page
     except(IndexError):
 
         all_questions = Question.objects.filter(quiz=quiz)
-        # Generates results of the quiz played
+
         quiz_results = request.session['quiz_results']
-        # del(request.session['quiz_results'])
-        # request.session.modified = True
+        del(request.session['quiz_results'])
+        request.session.modified = True
 
         return render(request, "quiz_results.html", {'quiz': quiz,
                                                      'all_questions': all_questions,
