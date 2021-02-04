2f9fc90bb38779379d1a00c5258e85983626aa44
Antoine Maurin <antoine.maurin26@gmail.com>
2021-02-04 18:44:22 +0100

    Essai de modification des settings de prod de la base de données.

--- Quiz_App/settings/production.py
+++ Quiz_App/settings/production.py
@@ -16,10 +16,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'purbeurre',
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': os.environ['DB_HOST'],
-        'PORT': os.environ['DB_PORT'],
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+
+        # 'USER': os.environ['DB_USER'],
+        # 'PASSWORD': os.environ['DB_PASSWORD'],
+        # 'HOST': os.environ['DB_HOST'],
+        # 'PORT': os.environ['DB_PORT'],
     }
 }
 
